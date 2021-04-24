373540657699f5eba22448245c8553a5b1eb837a
Kevin-Hageland-NTI-Johanneberg <kevin.hageland@elev.ga.ntig.se>
2021-04-24 21:47:51 +0200

    saturday v16

--- app.rb
+++ app.rb
@@ -66,11 +66,10 @@ get('/business/:id') do
   db = SQLite3::Database.new('db/online-investor.db')
   db.results_as_hash = true
 
-  business = db.execute("SELECT businesses.name
-  FROM ((user_to_business 
-    INNER JOIN users ON user_to_business.user_id = users.id)
-    INNER JOIN businesses ON user_to_business.user_id = user_id)
-    WHERE users.id = ?", id)
+  business = db.execute("SELECT businesses.id, businesses.name, businesses.money
+  FROM (user_to_business 
+    INNER JOIN businesses ON user_to_business.business_id = businesses.id)
+    WHERE user_id = ?", id)
   slim(:"management/businesses", locals:{business:business})
 end
 
@@ -78,14 +77,14 @@ get('/account/:id') do
   id = session[:id].to_i
   db = SQLite3::Database.new('db/online-investor.db')
   db.results_as_hash = true
-  username = db.execute("SELECT username FROM users WHERE id = ?", id).first #fix dis
-  session[:username] = username
+  user_data = db.execute("SELECT * FROM users WHERE id = ?", id).first
+  session[:username] = user_data["username"]
+  session[:money] = user_data["money"]
 
   business = db.execute("SELECT businesses.id, businesses.name, businesses.money
-  FROM ((user_to_business 
-    INNER JOIN users ON user_to_business.user_id = users.id)
-    INNER JOIN businesses ON user_to_business.user_id = user_id)
-    WHERE users.id = ?", id)
+  FROM (user_to_business 
+    INNER JOIN businesses ON user_to_business.business_id = businesses.id)
+    WHERE user_id = ?", id)
   slim(:"management/user", locals:{business:business})
 end
 
@@ -94,7 +93,7 @@ post('/change_username/:id') do
   new_username = params[:username]
 
   db = SQLite3::Database.new('db/online-investor.db')
-  db.execute("UPDATE users SET username = ? WHERE id = ?", new_username, id)
+  db.execute("UPDATE users SET username = ? WHERE id = ?", new_username, id).first
   redirect('/account/:id')
 end
 
@@ -112,14 +111,13 @@ post('/change_password/:id') do
   else #felhantering
     "Lösenordet matchade inte!"
   end
-
 end
 
 post('/add_account_money/:id') do
   id = session[:id].to_i
-  money_to_add = params[:money_to_add]
+  money_to_add = params[:money_to_add].to_i
   db = SQLite3::Database.new('db/online-investor.db')
-  current_money = db.execute("SELECT money FROM users WHERE id = ?", id).first
+  current_money = db.execute("SELECT money FROM users WHERE id = ?", id).first.first
   total_money = current_money + money_to_add
   db.execute('UPDATE users SET money = ? WHERE id = ?', total_money, id)
   redirect('/account/:id')
@@ -143,6 +141,48 @@ post('/add_money/:id') do
   end
 end
 
-post('/create post') do
+post('/leave/:id') do
+  id = params[:id].to_i
+  user_id = session[:id]
+  db = SQLite3::Database.new('db/online-investor.db')
+  db.execute("DELETE FROM user_to_business WHERE business_id = ? AND user_id = ?", id, user_id).first
+  redirect('/account/:id')
+end
+
+get('/logout') do
+  session[:id] = nil
+  redirect('/')
+end
+
+post('/join_business') do
+  id = session[:id]
+  business_name = params[:business_name]
+  db = SQLite3::Database.new('db/online-investor.db')
+  business_id = db.execute("SELECT id FROM businesses WHERE name = ?", business_name)
+  db.execute("INSERT INTO user_to_business (user_id, business_id) VALUES (?, ?)", id, business_id)
+  redirect('/account/:id')
+end
+
+post('/create_business') do
+  id = session[:id]
+  business_name = params[:business_name]
+  starting_budget = params[:starting_budget]
+  db = SQLite3::Database.new('db/online-investor.db')
+  user_money = db.execute('SELECT money FROM users WHERE id = ?', id).first.first
+
+  if user_money > starting_budget
+    user_money -= starting_budget
+    db.execute('UPDATE users SET money = ? WHERE id = ?', user_money, id)
+
+    db.execute("INSERT INTO businesses (name, money) VALUES (?, ?)", business_name, starting_budget)
+    business_id = db.execute("SELECT id FROM businesses WHERE name = ?", business_name).first.first
+    db.execute("INSERT INTO user_to_business (user_id, business_id) VALUES (?, ?)", id, business_id)
+    redirect('/account/:id')
+  else
+    "You don't have enough money on your account for that starting budget :("
+  end
+end
+
+get('/create_post') do
   slim(:"/management/new_post")
 end
